c2b1c714fc02f5fe64f82010c4028772717e844c
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-01-18 02:48:42 -0500

    MinGW support: Add patches-win32/05-libffi-latest.patch
    This commit integrates changes originally added to the project
    as python-cmake-buildsystem/python-cmake-buildsystem@9fdd1408b (Build the ctypes extension
    differently on Windows, add a patch to bring in the latest version
    of libffi) and python-cmake-buildsystem/python-cmake-buildsystem@025343fba (Add a missing
    patch to ctypes for windows)
    
    Co-authored-by: David Sansome <me@davidsansome.com>

--- Modules/_ctypes/callproc.c
+++ Modules/_ctypes/callproc.c
@@ -776,7 +776,7 @@ static int _call_function_pointer(int flags,
     ffi_cif cif;
     int cc;
 #ifdef MS_WIN32
-    int delta;
+    int delta = 0;
 #ifndef DONT_USE_SEH
     DWORD dwExceptionCode = 0;
     EXCEPTION_RECORD record;
@@ -827,7 +827,6 @@ static int _call_function_pointer(int flags,
 #ifndef DONT_USE_SEH
     __try {
 #endif
-        delta =
 #endif
                 ffi_call(&cif, (void *)pProc, resmem, avalues);
 #ifdef MS_WIN32
